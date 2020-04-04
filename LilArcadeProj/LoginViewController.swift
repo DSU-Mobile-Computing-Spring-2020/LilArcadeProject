7f899fce0cad10780827605092e5b9455b1a3278
Malon Holmes <itsmalonholmes@gmail.com>
2020-04-04 01:24:07 -0400

    added pod parse

---
+++ LilArcadeProj/LoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LoginViewController.swift
+//  LilArcadeProj
+//
+//  Created by JAMIE HOLMES on 4/4/20.
+//  Copyright © 2020 Malon Holmes. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    //edit....
+    /*
+     @IBOutlet weak var passwordField: UITextField!
+         @IBOutlet weak var usernameField: UITextField!
+         
+         override func viewDidLoad() {
+             super.viewDidLoad()
+
+             // Do any additional setup after loading the view.
+         }
+         
+         @IBAction func onSignUp(_ sender: Any) {
+     //        let user = PFUser()
+     //        user.username = usernameField.text
+     //        user.password = passwordField.text
+     //
+     //        user.signUpInBackground { (success, error) in
+     //            if success {
+     //                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+     //            } else {
+     //                print("Error: \(error?.localizedDescription)")
+     //            }
+     //
+     //        }
+
+         }
+         
+         @IBAction func onLogIn(_ sender: Any) {
+         }
+    
+    */
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
