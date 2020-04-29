594688dc192dce2e806fcfece44a0bfda645deec
Pudd <Pudd@Jonathan-Johnsons-MacBook-Pro.local>
2020-04-29 12:54:58 -0400

    PacMan Movement

--- LilArcadeProj/MazeViewController.swift
+++ LilArcadeProj/MazeViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 
 class MazeViewController: UIViewController {
 
+    @IBOutlet weak var pacImageView: UIImageView!
+    
+    @IBOutlet weak var exitImageView: UIImageView!
     
     @IBOutlet var ghost1: UIImageView!
     
@@ -19,8 +22,7 @@ class MazeViewController: UIViewController {
     @IBOutlet var ghost3: UIImageView!
     
     @IBOutlet var ghost4: UIImageView!
-    
-    @IBOutlet var exit: UIImageView!
+
     
     @IBOutlet var wall: [UIImageView]!
     
@@ -28,39 +30,45 @@ class MazeViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    var pacViewOrigin: CGPoint!
     
-    var imagearray = [UIImage(named: "ghost.png")!,]
-    
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-
+        addPanGesture(view: pacImageView)
+        pacViewOrigin = pacImageView.frame.origin
+        view.bringSubviewToFront(toFront: pacImageView)
         // Do any additional setup after loading the view.
-        
-        self.ghost1.animationImages = imagearray
-        self.ghost1.animationDuration = 12.0
-        self.ghost1.startAnimating()
-        
+}
+    func addPanGesture(view: UIView){
     
-        
-        
-       // add the rest of ghost once test
+        let pan = UIPanGestureRecognizer(target: self, action: #selector (MazeViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+    }
+    func handlePan(sender: UIPanGestureRecognizer) {
+    
+    let pacView = sender.view!
+    let translation = sender.translation(in: view)
+    
+        switch sender.state {
+        case.began, .changed:
+            pacView.center = CGPoint(x: pacView.center.x + translation.x, y: pacView.center.y + translation.y)
+            sender.setTranslation(CGPoint.zero, in: view)
+        case .ended:
+            if pacView.frame.intersects(exitImageView.frame){
+                UIView.animate(withDuration: 0.3) {
+                    self.pacImageView.alpha = 0.0
+                }
+            } else{
+                UIView.animate(withDuration: 0.3) {
+                    self.pacImageView.frame.origin = pacViewOrigin
+                }
+            }
+        default :
+            break
     }
     
-    @IBAction func didPanPacView(_ sender: UIPanGestureRecognizer) {
-        let location = sender.location(in: view)
-        print("Location: x: \(location.x), y: \(location.y)")
-        let pacView = sender.view!
-        pacView.center = location
-    }
     
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
 
 }
